0cdb7fe503481cdee3a5a261e2460bba0b923d46
nexocentric <nexocentric@gmail.com>
2013-09-17 08:56:25 +0900

    finished all tests pass

--- HtmlInterface.php
+++ HtmlInterface.php
@@ -5,6 +5,9 @@ class HtmlInterface
 	const TOP_TAG_NAME = "TOP_TAG_NAME";
 	const PROPERTIES = "PROPERTIES";
 
+	private $inParseCycle = false;
+	private $indentLevel = 0;
+	private $indentPattern = "\t";
 	private $selfClosingTagList = array(
 		"base", 
 		"basefont", 
@@ -24,25 +27,20 @@ class HtmlInterface
 	private $html = "";
 	private $childFragments = array();
 	
-	public function __construct($parentTag)
+	public function __construct($definition = "")
 	{
-		//initializations
-		$this->topTag[HtmlInterface::TOP_TAG_NAME] = "";
-		$this->topTag[HtmlInterface::PROPERTIES] = array();
-		
-		$properties = func_get_args();
-		array_splice($properties, 0, 1);
-		$this->topTag[HtmlInterface::TOP_TAG_NAME] = $parentTag;
-		$this->topTag[HtmlInterface::PROPERTIES] = $properties;
+		if(!empty($definition)) {
+			$this->doctype($definition);
+		}
 	}
 
 	//create a function for the html tag then save it
 	public function __call($tagName, $properties)
 	{
-		$this->html = $this->something($tagName, $properties);
+		return $this->html = $this->initializeTag($tagName, $properties);
 	}
 	
-	private function something($tagName, $properties)
+	private function initializeTag($tagName, $properties)
 	{
 		$attributes = array();
 		$innerText = "";
@@ -50,6 +48,14 @@ class HtmlInterface
 		$children = array();
 		
 		foreach($properties as $property) {
+			//since HtmlInterface has a to __toString method
+			//this has to be valuated before any
+			//string evaluations
+			if($property instanceof HtmlInterface) {
+				$property->clearDoctype();
+				$children[] = $property;
+				continue;
+			}
 			if(strpos($property, "\n") !== false) {
 				$children[] = $property;
 				continue;
@@ -57,16 +63,17 @@ class HtmlInterface
 			//inner text and children
 			if(is_string($property)) {
 				$stringList[] = $property;
+				continue;
 			}
 	    }
 
 	    //
 	    $stringCount = count($stringList);
-	    for ($nextString = 0; $nextString <= $stringCount; $nextString += 2) {
-	    	if($nextString == $stringCount) {
-	    		//$innerText = $stringList[$nextString];
-	    		break;
-	    	}
+	    if($stringCount % 2) {
+			$innerText = $stringList[$stringCount - 1];
+	    }
+	    
+	    for ($nextString = 0; ($nextString + 1) < $stringCount; $nextString += 2) {
 	    	$attributes[$stringList[$nextString]] = $stringList[$nextString + 1];
 	    }
 
@@ -85,29 +92,65 @@ class HtmlInterface
 		return $attributeString;
 	}
 
+	private function increaseIndent($childString)
+	{
+		$childList = substr_replace(
+			$childString,
+			"",
+			strrpos($childString, "\n")
+		);
+		$childList = explode("\n", $childList);
+		foreach($childList as $index => $child) {
+			$childList[$index] = $this->indent() . $child;
+		}
+		return implode("\n", $childList) . "\n";
+		return $childString;
+	}
+	
+	//this function is probably going to cause you lots of problems right now...
+	// all fixing needs to happen here
 	public function parseChildren($children)
 	{
 		$childString = "";
+		$newline = $this->inParseCycle ? "" : "\n";
 		if(empty($children)) {
 			return $childString;
 		}
+		if(!is_array($children)) {
+			$children = array($children);
+		}
+		$currentIndentLevel = $this->indentLevel;
 		foreach($children as $child) {
+			if($child instanceof HtmlInterface) {
+				$this->inParseCycle = true;
+				$this->indentLevel = 0;
+				$child = $this->parseChildren((string)$child);
+				$this->indentLevel = $currentIndentLevel;
+				$this->inParseCycle = false;
+			}
+			$child = $this->increaseIndent($child);
 			$childString .= $child;
 		}
-		return $childString;
+		return $newline . $childString;
 	}
 
 	public function createTag($tagName, $attributes, $innerText = "", $children = "")
 	{
 		$createdTag = "";
-
+		
 		//
 		if(in_array($tagName, $this->selfClosingTagList)) {
 			//
-			$createdTag = "<$tagName%s>\n%s%s";
+			if($children) {
+				$createdTag = "<$tagName%s>%s%s";
+			} else {
+				$createdTag = "<$tagName%s>\n%s%s";
+			}
+			$this->indentLevel = 0;
 		} else {
 			//
 			$createdTag = "<$tagName%s>%s%s</$tagName>\n";
+			$this->indentLevel = 1;
 		}
 
 		$createdTag = sprintf(
@@ -116,37 +159,88 @@ class HtmlInterface
 			$innerText,
 			$this->parseChildren($children)
 		);
-
 		
 		return $createdTag;
 	}
 
+	protected function clearDoctype()
+	{
+		$this->doctype = "";	
+	}
+	
 	public function doctype($definition) {
 		$this->doctype = sprintf(
 			"<!DOCTYPE %s>\n",
 			$definition
 		);
 	}
+	
+	public function repeat(HtmlInterface $html, $count)
+	{
+		$html->clearDoctype();
+		return str_repeat($html, $count);
+	}
+	
+	public function newline() {
+		return "\n";
+	}
+	
+	private function indent()
+	{
+		return str_repeat($this->indentPattern, $this->indentLevel);
+	}
 
 	public function __toString()
 	{
-		if(!empty($this->html)) {
-			$this->topTag[HtmlInterface::PROPERTIES][] = $this->html;
-		}
-		$html = $this->something(
-			$this->topTag[HtmlInterface::TOP_TAG_NAME],
-			$this->topTag[HtmlInterface::PROPERTIES]
-		);
-		return $this->doctype . $html;
+		return $this->doctype . $this->html;
 	}
 
 	# comment tag
 	public function comment($comment) {
-		echo "\n", $this->indent(), "<!-- $comment -->\n";
-		$this->outdent();
+		return "<!-- $comment -->\n";
 	}
 }
 
-$tag = new HtmlInterface("meta");
-$tag->meta();
-$something = (string)$tag;
+$body = new HtmlInterface("html //definitions");
+$body->body();
+
+$html = new HtmlInterface("html //definitions");
+$html->html($html->repeat($body, 2));
+1 + 1;
+
+class HtmlGen {
+  
+  static public $self_closing_tags = array(
+    "base", "basefont", "br", "col", "frame", "hr", "input", "link", "meta", "param"
+  );
+  
+  static private $indent_level = -1;
+  static private $indent_pattern = "\t";
+  
+  static private $cycles = array();
+  
+  # cycler
+  static public function cycle(Array $options, $handle="default"){
+    if(!array_key_exists($handle, self::$cycles)){
+      self::$cycles[$handle] = $options;
+      return current(self::$cycles[$handle]);
+    }
+    else {
+      if($ret = next(self::$cycles[$handle])){
+        return $ret;
+      }
+      else {
+        reset(self::$cycles[$handle]);
+        return current(self::$cycles[$handle]);
+      }
+    }
+  }
+  
+  # reset specific cycle
+  static public function reset_cycle($handle="default"){
+    if(array_key_exists($handle, self::$cycles)){
+      reset(self::$cycles[$handle]);
+    }
+  }
+  
+}
--- tests/HtmlOutputTest.php
+++ tests/HtmlOutputTest.php
@@ -18,14 +18,42 @@ class HtmlOutputTest extends PHPUnit_Framework_TestCase
 
 	/** 
     * @test
+    */
+	public function newline()
+	{
+		$html = new HtmlInterface();
+		$this->assertSame(
+			"\n", 
+			$html->newline(), 
+			"newline creation failed"
+		);
+	}
+
+	/** 
+	* @test
+	*/
+	public function comment()
+	{
+		$html = new HtmlInterface();
+		$this->assertSame(
+			"<!-- this is a comment -->\n",
+			$html->comment("this is a comment"),
+			"Failed to convert interface to string."
+		);
+
+	}
+
+	/** 
+    * @test
     * @depends toString
     */
 	public function selfClosingTag()
 	{
-		$br = new HtmlInterface("br");
+		$html = new HtmlInterface();
+		$html->br();
 		$this->assertSame(
 			"<br>\n",
-			(string)$br,
+			(string)$html,
 			"Failed to return br tag as parent (self-closing)."
 		);
 	}
@@ -36,10 +64,11 @@ class HtmlOutputTest extends PHPUnit_Framework_TestCase
     */
 	public function regularTag()
 	{
-		$div = new HtmlInterface("div");
+		$html = new HtmlInterface();
+		$html->div();
 		$this->assertSame(
 			"<div></div>\n",
-			(string)$div,
+			(string)$html,
 			"Failed to return div tag as parent."
 		);
 	}
@@ -50,12 +79,37 @@ class HtmlOutputTest extends PHPUnit_Framework_TestCase
 	*/
 	public function returnsDoctype()
 	{
-		$html = new HtmlInterface("html");
-		$html->doctype("html //definitions");
+		$html = new HtmlInterface("html //definitions");
+		$this->assertSame(
+			"<!DOCTYPE html //definitions>\n", 
+			(string)$html, 
+			"Failed to set solidary !DOCTYPE."
+		);
+
+		$html = new HtmlInterface("html //definitions");
+		$html->html();
 		$this->assertSame(
 			"<!DOCTYPE html //definitions>\n<html></html>\n", 
 			(string)$html, 
-			"Failed to set !DOCTYPE."
+			"Failed to set !DOCTYPE with document HTML."
+		);
+	}
+
+	/** 
+	* @test
+	* @depends regularTag
+	*/
+	public function multipleSecondLevelTags()
+	{
+		$html = new HtmlInterface();
+		$html->html(
+			$html->head(),
+			$html->body()
+		);
+		$this->assertSame(
+			"<html>\n\t<head></head>\n\t<body></body>\n</html>\n",
+			(string)$html,
+			"Failed to return div tag as parent."
 		);
 	}
 
@@ -65,10 +119,11 @@ class HtmlOutputTest extends PHPUnit_Framework_TestCase
     */
 	public function selfClosingTagWithAttributes()
 	{
-		$meta = new HtmlInterface("meta", "content", "content-text");
+		$html = new HtmlInterface();
+		$html->meta("content", "content-text");		
 		$this->assertSame(
 			"<meta content=\"content-text\">\n",
-			(string)$meta,
+			(string)$html,
 			"Failed to return br tag as parent (self-closing)."
 		);
 	}
@@ -80,10 +135,11 @@ class HtmlOutputTest extends PHPUnit_Framework_TestCase
     */
 	public function regularTagWithAttributes()
 	{
-		$div = new HtmlInterface("div", "class", "classname");
+		$html = new HtmlInterface();
+		$html->div("class", "classname");
 		$this->assertSame(
 			"<div class=\"classname\"></div>\n",
-			(string)$div,
+			(string)$html,
 			"Failed to return div tag as parent."
 		);
 	}
@@ -94,11 +150,13 @@ class HtmlOutputTest extends PHPUnit_Framework_TestCase
     */
 	public function selfClosingTagWithSiblings()
 	{
-		$meta = new HtmlInterface("meta");
-		$meta->meta();
+		$html = new HtmlInterface();
+		$html->meta(
+			$html->meta()
+		);
 		$this->assertSame(
 			"<meta>\n<meta>\n",
-			(string)$meta,
+			(string)$html,
 			"Failed to return br tag as parent (self-closing)."
 		);
 	}
@@ -109,13 +167,15 @@ class HtmlOutputTest extends PHPUnit_Framework_TestCase
     */
 	public function regularTagWithChildren()
 	{
-		$div = new HtmlInterface("div");
-		$div->span(
-			$div->p()
+		$html = new HtmlInterface();
+		$html->div(
+			$html->span(
+				$html->p()
+			)
 		);
 		$this->assertSame(
-			"<div>\n\t<span>\n\t\t<p></p>\n\t</span></div>\n",
-			(string)$div,
+			"<div>\n\t<span>\n\t\t<p></p>\n\t</span>\n</div>\n",
+			(string)$html,
 			"Failed to return div tag as parent."
 		);
 	}
@@ -126,10 +186,15 @@ class HtmlOutputTest extends PHPUnit_Framework_TestCase
     */
 	public function selfClosingTagWithAttributesAndSiblings()
 	{
-		$meta = new HtmlInterface("meta", "content", "content-text");
+		$html = new HtmlInterface();
+		$html->meta(
+			"content",
+			"content-text",
+			$html->meta()
+		);
 		$this->assertSame(
-			"<meta content=\"content-text\">\n",
-			(string)$meta,
+			"<meta content=\"content-text\">\n<meta>\n",
+			(string)$html,
 			"Failed to return br tag as parent (self-closing)."
 		);
 	}
@@ -140,67 +205,163 @@ class HtmlOutputTest extends PHPUnit_Framework_TestCase
     */
 	public function regularTagWithAttributesAndChildren()
 	{
-		$div = new HtmlInterface("div", "class", "classname");
+		$html = new HtmlInterface();
+		$html->div(
+			"class", 
+			"classname",
+			$html->p("something"),
+			$html->p("nothing")
+		);
 		$this->assertSame(
-			"<div class=\"classname\"></div>\n",
-			(string)$div,
+			"<div class=\"classname\">\n\t" .
+			"<p>something</p>\n\t" .
+			"<p>nothing</p>\n" .
+			"</div>\n",
+			(string)$html,
 			"Failed to return div tag as parent."
 		);
 	}
 
 	/** 
     * @test
-    * @depends regularTag
+    * @depends comment
+    * @depends newline
+    * @depends regularTagWithAttributesAndChildren
     */
 	public function documentFragment()
 	{
-		$div = new HtmlInterface("div");
+		$html = new HtmlInterface();
+		$html->div(
+			$html->comment("this works"),
+			$html->h1(),
+			$html->newline(),
+			$html->span(
+				$html->p(
+				)
+			)
+		);
 		$this->assertSame(
-			"<div>\n\t<span>\n\t\t<p></p>\n\t</span>\n</div>\n",
-			(string)$div,
+			"<div>\n\t<!-- this works -->\n\t<h1></h1>\n\t\n\t<span>\n\t\t<p></p>\n\t</span>\n</div>\n",
+			(string)$html,
 			"Failed to return div tag as parent."
 		);
 	}
 
 	/** 
     * @test
-    * @depends documentFragment
+    * @depends regularTagWithAttributesAndChildren
     */
-	public function minimalDocument()
+	public function completeDocument()
 	{
 		//setup
 		$html = new HtmlInterface("html");
-		$head = new HtmlInterface("head");
-		$body = new HtmlInterface("body");
-		
-		$this->assertTrue(false, "this test is unfinished");
-		/*$html->body();
-		$this->assertSame(
-			"<html>\n\t<body></body>\n</html>\n",
-			(string)$html,
-			"Failed to return html tag as parent."
+		$html->html(
+			$html->head(
+				$html->meta("name", "description")
+			),
+			$html->body(
+				$html->p("document")
+			)
 		);
-
-		$head->meta();
 		$this->assertSame(
-			"<head>\n\t<meta></head>\n",
-			(string)$head,
-			"Failed to return head tag as parent."
-		);
+			"<!DOCTYPE html>\n" .
+			"<html>\n" .
+			"\t<head>\n" .
+			"\t\t<meta name=\"description\">\n" .
+			"\t</head>\n" .
+			"\t<body>\n" . 
+			"\t\t<p>document</p>\n" .
+			"\t</body>\n" .
+			"</html>\n", 
+			(string)$html)
+		;
+	}
 
-		$body->p();
+	/**
+	* @test
+	* @depends returnsDoctype
+	* @depends documentFragment
+	*/
+	public function clearDoctype()
+	{
+		$body = new HtmlInterface("html //definitions");
+		$body->body();
+
+		$html = new HtmlInterface("html //definitions");
+		$html->html($body);
 		$this->assertSame(
-			"<body>\n\t<p></p>\n</body>\n",
-			(string)$body,
-			"Failed to return body tag as parent."
+			"<!DOCTYPE html //definitions>\n<html>\n\t<body></body>\n</html>\n", 
+			(string)$html, 
+			"Failed to set !DOCTYPE with document HTML."
 		);
+	}
 
+	/** 
+    * @test
+    * @depends clearDoctype
+    * @depends documentFragment
+    * @depends completeDocument
+    */
+	public function completeDocumentFromFragments()
+	{
+		//setup
+		$html = new HtmlInterface("html");
+		$head = new HtmlInterface();
+		$head->head(
+				$head->meta("name", "description")
+		);
+		$body = new HtmlInterface();
+		$body->body(
+			$body->p("document")
+		);
+		$html->html(
+			$head,
+			$body
+		);
 		$this->assertSame(
-			"<div>\n\t<span>\n\t\t<p></p>\n\t</span>\n</div>\n",
-			(string)$div,
-			"Failed to return div tag as parent."
+			"<!DOCTYPE html>\n" .
+			"<html>\n" .
+			"\t<head>\n" .
+			"\t\t<meta name=\"description\">\n" .
+			"\t</head>\n" .
+			"\t<body>\n" . 
+			"\t\t<p>document</p>\n" .
+			"\t</body>\n" .
+			"</html>\n", 
+			(string)$html
 		);
+	}
 
-		*/
+	/** 
+    * @test
+    * @depends completeDocumentFromFragments
+    */
+	public function repeatFragment()
+	{
+		$div = new HtmlInterface();
+		$div->div(
+			$div->div(
+				$div->p()
+			)
+		);
+		$html = new HtmlInterface();
+		$html->html(
+			$html->repeat($div, 2)
+		);
+		$this->assertSame(
+			"<html>\n" .
+			"\t<div>\n" .
+			"\t\t<div>\n" .
+			"\t\t\t<p></p>\n" .
+			"\t\t</div>\n" .
+			"\t</div>\n" .
+			"\t<div>\n" .
+			"\t\t<div>\n" .
+			"\t\t\t<p></p>\n" .
+			"\t\t</div>\n" .
+			"\t</div>\n" .
+			"</html>\n", 
+			(string)$html
+		);
 	}
 }
